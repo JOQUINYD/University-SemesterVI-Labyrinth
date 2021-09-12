e5a1c1b2e5149768b95dc2c33adc4c5e71058ca5
Jose Gamboa <joalgamro2010@gmail.com>
2021-09-12 00:52:29 -0600

    U: Main

--- main.c
+++ main.c
@@ -37,7 +37,36 @@ Box** crateMatrix(int rows, int cols){
     return matrix;
 }
 
+Box** crateSharedMatrix(int rows, int cols){
+    Box** matrix = mmap(NULL, rows * sizeof *matrix, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0); 
 
+
+    for (int i=0; i<rows; i++)
+    {
+        matrix[i] = mmap(NULL, cols * sizeof *matrix[i], PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0); 
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i][j] = *newSharedBox('*');
+
+        }
+        
+    }
+    matrix[0][0].marked = true;
+    matrix[0][0].down = true;
+
+    matrix[0][0].type = ' ';
+    matrix[1][0].type = ' ';
+    matrix[1][1].type = ' ';
+    matrix[2][1].type = ' ';
+    matrix[3][1].type = ' ';
+    matrix[3][2].type = ' ';
+    matrix[4][2].type = ' ';   
+    return matrix;
+}
 
 int main(int argc, char *argv[]){
 
@@ -45,8 +74,24 @@ int main(int argc, char *argv[]){
     int cols = 5;
     int rows = 5;
 
+     ///////////////////////////////////////////////////////////////////////////
+
+    //Thread execution
+    printf("///////////////////////////////////////////////////////////////////////////\n");
+    printf("Inicia Ejecucion Threads\n");
+    sleep(1);
+
     //Get the Matrix
     Box** matrixThreads = crateMatrix(rows,cols); 
+
+    //Declare Printer Info
+    bool *finishedThreads = malloc(sizeof finishedThreads);
+    *finishedThreads = false;
+    PrinterInfo *printerInfoThreads = newPrinterInfo(matrixThreads,rows,cols,finishedThreads);
+
+    //Create PrinterThread
+    pthread_t printerThreads;
+    pthread_create(&printerThreads, NULL, &printMatrix, printerInfoThreads);
     
     //SetVariables
     setVariables(matrixThreads,rows,cols); 
@@ -58,32 +103,51 @@ int main(int argc, char *argv[]){
     pthread_t t1;
     pthread_create(&t1, NULL, &executeThread, startPathThread);
 
-    //Declare Printer Info
-    bool *finished = malloc(sizeof finished);
-    *finished = false;
-    PrinterInfo *printerInfo = newPrinterInfo(matrixThreads,rows,cols,finished);
-
-    //Create PrinterThread
-    pthread_t printerThread;
-    pthread_create(&printerThread, NULL, &printMatrix, printerInfo);
-
     //Join the PathFinderExecute Thread
     pthread_join(t1, NULL);
     
     //Finish Printer Thread
-    *finished = true;
+    *finishedThreads = true;
     
     //Join Printer Thread
-    pthread_join(printerThread, NULL);
+    pthread_join(printerThreads, NULL);
     
-    
-
-
-
-
 
     
-    printf("Terminé crack\n");
+    ///////////////////////////////////////////////////////////////////////////
+
+    //Fork Execution 
+    printf("///////////////////////////////////////////////////////////////////////////\n");
+    printf("Inicia Ejecucion Forks\n");
+    sleep(1);
+
+    //Get the Matrix
+    Box** matrixForks = crateSharedMatrix(rows,cols); 
+    
+    //Declare Printer Info
+    bool *finishedForks = mmap(NULL, sizeof finishedForks, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0); 
+
+    *finishedForks = false;
+    PrinterInfo *printerInfoForks = newPrinterInfo(matrixForks,rows,cols,finishedForks);
+
+   
+    //SetVariables
+    setVariables(matrixForks,rows,cols); 
+    
+    //Create first Path
+    Path* startPathFork = newPath(0,0,'d',0);
+
+    //Fork the execution and Printer
+    if (fork()==0)
+    {
+        executeFork(startPathFork);
+        wait();
+        *finishedForks = true;
+
+    }
+    else{
+        printMatrix(printerInfoForks);
+    }
 
     return 0;
 }
